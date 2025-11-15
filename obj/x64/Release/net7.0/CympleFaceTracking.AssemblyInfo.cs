7193a33c8f37b613a640570b6cf8994e941d7696
Dominocs <45429745+Dominocs@users.noreply.github.com>
2025-11-15 22:32:15 +0800

    1.3.2

--- obj/x64/Release/net7.0/CympleFaceTracking.AssemblyInfo.cs
+++ obj/x64/Release/net7.0/CympleFaceTracking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CympleFaceTracking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264d767811752e743ec4a3a55c142d8974b8b3fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224f4f92867cac1eecff2d5f67daa4c45d0e0661")]
 [assembly: System.Reflection.AssemblyProductAttribute("CympleFaceTracking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CympleFaceTracking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
